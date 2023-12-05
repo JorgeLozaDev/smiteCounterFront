cc16be728896dde124cfae1c1de7a0ad38e96561
jorge <j0rg3e93@gmail.com>
2023-12-05 12:38:44 +0100

    Se crea la estructura base del menu

--- src/common/Header/Header.jsx
+++ src/common/Header/Header.jsx
@@ -1,7 +1,40 @@
-import React from 'react'
+import React from "react";
+import { Navbar, Container, Nav, NavDropdown, Image } from "react-bootstrap";
 
 export const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" fixed="top">
+        <Container>
+          <Navbar.Brand href="/">
+            <Image src="../../../public/logo2.png" className="logo" fluid />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto"></Nav>
+
+            <Nav>
+              <NavDropdown title="Menú" id="collasible-nav-dropdown">
+                <NavDropdown.Item href="/profile">Perfil</NavDropdown.Item>
+
+                <NavDropdown.Item href="/mettings">Citas</NavDropdown.Item>
+                <NavDropdown.Divider />
+
+                <NavDropdown.Item href="/admin/listusers">
+                  Lista de usuarios
+                </NavDropdown.Item>
+
+                <NavDropdown.Item>Log out</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+
+            <Nav>
+              <Nav.Link href="/login">Login</Nav.Link>
+              <Nav.Link href="/singup">Singup</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
+};
