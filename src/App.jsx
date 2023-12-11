b5dec611d31fffeb8e246d392992cff13c943476
jorge <j0rg3e93@gmail.com>
2023-12-11 11:11:36 +0100

    Se crea el componente del footer y se imporat en app

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 import { Header } from "./common/Header/Header";
 import { Body } from "./pages/Body/Body";
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import { Footer } from "./pages/Footer/Footer";
 
 function App() {
   return (
@@ -10,6 +10,7 @@ function App() {
       <div className="margeMenuSuperior">
         <Header />
         <Body />
+        <Footer />
       </div>
     </>
   );
