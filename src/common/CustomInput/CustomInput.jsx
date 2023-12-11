65aab70d508d33f0b77199352072c41898724e19
jorge <j0rg3e93@gmail.com>
2023-12-11 10:08:32 +0100

    Se crea el componente de customInput

---
+++ src/common/CustomInput/CustomInput.jsx
@@ -0,0 +1,29 @@
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
+
+const Input = ({ placeholder, type, name, handler, value, disabled,max,min,className }) => {
+  const handleChange = (event) => {
+    const { value } = event.target;
+    handler(value, name);
+  };
+
+  return (
+    <>
+      <InputGroup className="mb-3">
+        <Form.Control
+          placeholder={placeholder}
+          type={type}
+          name={name}
+          value={value}
+          max={max}
+          min={min}
+          onChange={(e) => handleChange(e)}
+          className={className}
+          disabled={disabled}
+        />
+      </InputGroup>
+    </>
+  );
+};
+
+export default Input;
