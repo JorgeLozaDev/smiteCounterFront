2e2b9015268633eef6af0de45913c787093a1786
jorge <j0rg3e93@gmail.com>
2023-12-11 13:13:50 +0100

    Se crea la estructura base del footer

--- src/pages/Footer/Footer.jsx
+++ src/pages/Footer/Footer.jsx
@@ -1,7 +1,37 @@
-import React from 'react'
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import "./Footer.css"
 
 export const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <Container className="footer">
+      <Row>
+        <Col>
+          <p>SMITECOUNTER</p>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste, quo
+            facere? Minima saepe voluptatum porro officia, hic soluta quis atque
+            quod architecto laborum est itaque facere magni dicta rem facilis!
+          </p>
+        </Col>
+        <Col>
+          <p>Nuestros link</p>
+          <ul>
+            <li>
+              <a> Dioses</a>
+            </li>
+            <li>
+              <a> Counters</a>
+            </li>
+            <li>
+              <a> Contacto</a>
+            </li>
+          </ul>
+        </Col>
+        <Col>
+          <p>Redes sociales</p>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
