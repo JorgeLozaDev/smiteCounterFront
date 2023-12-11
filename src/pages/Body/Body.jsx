94128696aa96a3436612bd3cddb088f0a3b2617a
jorge <j0rg3e93@gmail.com>
2023-12-11 22:08:11 +0100

    Se actualiza el body

--- src/pages/Body/Body.jsx
+++ src/pages/Body/Body.jsx
@@ -1,11 +1,15 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import { Home } from "../Home/Home";
+import { Login } from "../Login/Login";
+import { SingIn } from "../SignIn/SingIn";
 
 export const Body = () => {
   return (
     <Routes>
       <Route path="*" element={<Navigate to="/" />} />
       <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/singin" element={<SingIn />} />
     </Routes>
   );
 };
