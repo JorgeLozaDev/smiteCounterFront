498b5fba666a6f0f11c2f3b273e43a4ee585b873
jorge <j0rg3e93@gmail.com>
2023-12-11 22:18:52 +0100

    Se agrega logica para el singin

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import { Button } from 'react-bootstrap'
+import { useNavigate } from 'react-router-dom'
 
 export const Login = () => {
+    const navigate = useNavigate()
   return (
-    <div>Login</div>
+    <div>Login
+        <Button onClick={()=>{navigate("/singin")}}>Crea cuenta</Button>
+    </div>
   )
 }
--- src/pages/SignIn/SingIn.jsx
+++ src/pages/SignIn/SingIn.jsx
@@ -1,7 +1,95 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Button, Col, Container, Form, Row } from "react-bootstrap";
+import Input from "../../common/CustomInput/CustomInput";
 
 export const SingIn = () => {
+  const [signindata, setSinginData] = useState({
+    name: "",
+    lastname: "",
+    email: "",
+    password: "",
+    username: "",
+    birthday: "",
+  });
+  const inputHandler = (value, name) => {
+    setSinginData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handlerSend = (event) => {
+    // insertUser("user/singup", logindata)
+    //   .then((date) => {
+    //     loginUser("user/login", logindata)
+    //       .then((dat) => {
+    //         dispatch(login({ credentials: dat.data.token }));
+    //         navigate("/profile");
+    //       })
+    //       .catch((e) => {
+    //         console.log(e);
+    //         handleServerError(e);
+    //       });
+    //   })
+    //   .catch((e) => {
+    //     console.log(e.response.data);
+    //     handleServerError(e);
+    //   });
+    event.preventDefault();
+  };
   return (
-    <div>SingIn</div>
-  )
-}
+    <>
+      <Container>
+        <Row>
+          <Col>
+            <Form onSubmit={handlerSend} method="post">
+              <Input
+                placeholder={"Nombre"}
+                type={"text"}
+                name={"name"}
+                handler={inputHandler}
+              />
+
+              <Input
+                placeholder={"Apellidos"}
+                type={"text"}
+                name={"lastname"}
+                handler={inputHandler}
+              />
+              <Input
+                placeholder={"Email"}
+                type={"email"}
+                name={"email"}
+                handler={inputHandler}
+              />
+              <Input
+                placeholder={"password"}
+                type={"password"}
+                name={"password"}
+                handler={inputHandler}
+              />
+              <Input
+                placeholder={"Nombre de usuario"}
+                type={"text"}
+                name={"username"}
+                handler={inputHandler}
+              />
+              <Input
+                placeholder={"Fecha naciemiento"}
+                type={"date"}
+                name={"birthday"}
+                handler={inputHandler}
+              />
+
+              <p className="text-center">
+                <Button type="submit" variant="secondary">
+                  Enviar
+                </Button>
+              </p>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
+};
