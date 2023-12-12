46659f7d6506d2e63a3f531169c6c193e829d2e5
jorge <j0rg3e93@gmail.com>
2023-12-12 10:40:59 +0100

    Se crea customToasty para mostrar de forma más dinamica los mensajes

---
+++ src/common/CustomToasty/CustomToasty.jsx
@@ -0,0 +1,63 @@
+// src/common/toasty/Toasty.js
+import React from "react";
+import { toast, ToastContainer as BaseToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+// Componente para mostrar tostadas
+const Toasty = ({ message, type = "default", options }) => {
+  const toastOptions = {
+    position: "top-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "light",
+    ...options,
+  };
+
+  const showToast = () => {
+    switch (type) {
+      case "info":
+        toast.info(message, toastOptions);
+        break;
+      case "success":
+        toast.success(message, toastOptions);
+        break;
+      case "warn":
+        toast.warn(message, toastOptions);
+        break;
+      case "error":
+        toast.error(message, toastOptions);
+        break;
+      default:
+        toast(message, toastOptions);
+        break;
+    }
+  };
+
+  showToast(); // Llamar al método de mostrar tostadas
+
+  return null; // Toasty component doesn't render anything in the DOM
+};
+
+// Componente de contenedor de tostadas
+const ToastContainer = () => {
+  return (
+    <BaseToastContainer
+      position="top-right"
+      autoClose={5000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="light"
+    />
+  );
+};
+
+export { Toasty, ToastContainer };
