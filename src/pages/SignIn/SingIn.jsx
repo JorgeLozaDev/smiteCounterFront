bbece39f55b16dda35d465970d8b29b0ef7e6a49
jorge <j0rg3e93@gmail.com>
2023-12-12 13:04:48 +0100

    Se ajusta el singIn para controlar todos los distintos tipos de error

--- src/pages/SignIn/SingIn.jsx
+++ src/pages/SignIn/SingIn.jsx
@@ -2,17 +2,18 @@ import React, { useState } from "react";
 import { Button, Col, Container, Form, Row } from "react-bootstrap";
 import Input from "../../common/CustomInput/CustomInput";
 import { singInUser } from "../../services/apiCalls";
-import { ToastContainer, Toasty } from "../../common/CustomToasty/CustomToasty";
+import { Toasty, ToastContainer } from "../../common/CustomToasty/CustomToasty";
+import { useNavigate } from "react-router-dom";
 
 export const SingIn = () => {
   const [signindata, setSinginData] = useState({
-    name: "",
-    lastname: "",
     email: "",
     password: "",
     username: "",
     birthday: "",
   });
+  const navigate = useNavigate();
+
   const inputHandler = (value, name) => {
     setSinginData((prevData) => ({
       ...prevData,
@@ -21,32 +22,53 @@ export const SingIn = () => {
   };
 
   const handlerSend = (event) => {
-    // insertUser("user/singup", logindata)
-    //   .then((date) => {
-    //     loginUser("user/login", logindata)
-    //       .then((dat) => {
-    //         dispatch(login({ credentials: dat.data.token }));
-    //         navigate("/profile");
-    //       })
-    //       .catch((e) => {
-    //         console.log(e);
-    //         handleServerError(e);
-    //       });
-    //   })
-    //   .catch((e) => {
-    //     console.log(e.response.data);
-    //     handleServerError(e);
-    //   });
+    event.preventDefault();
+
+    const requiredFields = ["email", "password", "username", "birthday"];
+    const emptyField = requiredFields.find((field) => !signindata[field]);
+
+    if (emptyField) {
+      Toasty({
+        message: `Por favor, completa el campo ${emptyField}`,
+        type: "error",
+      });
+      return;
+    }
+
     singInUser("user/addUser", signindata)
       .then((data) => {
-        console.log(data);
+        Toasty({
+          message: `${data.data.message} ...logueando`,
+          type: "success",
+        });
+        setTimeout(() => {
+          navigate("/login");
+        }, 3500);
       })
-      .catch((e) => {
-        Toasty({ message: e, type: "error" });
+      .catch((error) => {
+        // Manejar el error de Axios
+        if (error.response) {
+          // El servidor respondió con un código de estado diferente de 2xx
+          Toasty({
+            message: `Error: ${error.response.status} - ${error.response.data.message}`,
+            type: "error",
+          });
+        } else if (error.request) {
+          // La solicitud fue hecha, pero no se recibió una respuesta
+          Toasty({
+            message: "No se recibió respuesta del servidor",
+            type: "error",
+          });
+        } else {
+          // Algo sucedió al configurar la solicitud que desencadenó un error
+          Toasty({
+            message: "Error al configurar la solicitud",
+            type: "error",
+          });
+        }
       });
-
-    event.preventDefault();
   };
+
   return (
     <>
       <ToastContainer />
