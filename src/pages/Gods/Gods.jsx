6ba7ec7951e42908d4c7acd40bfc17731b243093
jorge <j0rg3e93@gmail.com>
2023-12-28 09:29:08 +0100

    UPDATE: En page gods, se hace la llamada a la api y se pinta

--- src/pages/Gods/Gods.jsx
+++ src/pages/Gods/Gods.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import { allGodsActives } from "../../services/apiCalls";
 
 const Gods = () => {
-  return (
-    <div>Gods</div>
-  )
-}
+  const [gods, setGods] = useState({});
 
-export default Gods
+  useEffect(() => {
+    allGodsActives("gods/allGodsActive")
+      .then((data) => {
+        setGods(data.data.allGodsActive);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  return (
+    <>
+      {gods.length > 0 ? (
+        <div>
+          {gods.map((god) => {
+            return (
+              <div key={god._id}>
+                <p>{god.name}</p>
+              </div>
+            );
+          })}
+        </div>
+      ) : (
+        <div><p>Sin datos</p></div>
+      )}
+    </>
+  );
+};
+
+export default Gods;
