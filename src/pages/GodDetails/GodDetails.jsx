dabcb37e2552dbe6495665c18b55c69047700d6a
jorge <j0rg3e93@gmail.com>
2024-01-02 10:52:24 +0100

    ADD - UPDATE: Se agregan imagenes de los dioses para usar en sus detalles, se ajusta el banner de la vista detalle del dios

--- src/pages/GodDetails/GodDetails.jsx
+++ src/pages/GodDetails/GodDetails.jsx
@@ -3,6 +3,7 @@ import { godDetails } from "../godSlice";
 import { useEffect, useState } from "react";
 import { godsDetails } from "../../services/apiCalls";
 import { Col, Container, Row } from "react-bootstrap";
+import "./GodDetails.css";
 
 const GodDetails = () => {
   const idGod = useSelector(godDetails);
@@ -22,134 +23,126 @@ const GodDetails = () => {
   return (
     <>
       {god ? (
-        <Container>
-          <Row>
-            <Col>
-              <img src={god.images.main} className="img-fluid" />
-            </Col>
-          </Row>
-          <Row className="caja">
-            <Col md={6}>
-              <img src={god.images.card} />
-            </Col>
-            <Col md={6}>
-              <div>
-                <p>{god.name}</p>
-              </div>
-            </Col>
-          </Row>
-          <Row className="caja">
-            <Col md={6}>
-              <div>
-                <p>{god.pantheon}</p>
-                <p>{god.role}</p>
-              </div>
-              <p>{god.lore}</p>
-            </Col>
-            <Col md={6}>
-              <img src={god.images.card} />
-            </Col>
-          </Row>
-          <Row className="caja">
-            <Col md={6}>
-              <img src={god.images.card} />
-            </Col>
-            <Col md={6}>
-              <p>{god.abilities[0].name}</p>
-              <p>{god.abilities[0].description}</p>
-              {god.abilities[0].details.length > 0 ? <div></div> : <p></p>}
-            </Col>
-          </Row>
-          <Row className="caja">
-            <Col md={6}>
-              <p>{god.abilities[1].name}</p>
-              <p>{god.abilities[1].description}</p>
-              {god.abilities[1].details.length > 0 ? (
+        <>
+          <Container
+            fluid
+            className="banner bannerGod"
+            style={{ backgroundImage: `url(${god.images.main})` }}
+          ></Container>
+          <Container className="py-5">
+            <Row className="caja">
+              <Col md={6}>
                 <div>
-                  {god.abilities[1].details.map((det, indice) => {
-                    return (
-                      <p key={indice}>
-                        {det.label}: {det.value}
-                      </p>
-                    );
-                  })}
+                  <p>{god.pantheon}</p>
+                  <p>{god.role}</p>
                 </div>
-              ) : (
-                <p></p>
-              )}
-            </Col>
-            <Col md={6}>
-              <img src={god.images.card} />
-            </Col>
-          </Row>
+                <p>{god.lore}</p>
+              </Col>
+              <Col md={6}>
+                <img src={god.images.card} />
+              </Col>
+            </Row>
+            <Row className="caja">
+              <Col md={6}>
+                <img src={god.images.card} />
+              </Col>
+              <Col md={6}>
+                <p>{god.abilities[0].name}</p>
+                <p>{god.abilities[0].description}</p>
+                {god.abilities[0].details.length > 0 ? <div></div> : <p></p>}
+              </Col>
+            </Row>
+            <Row className="caja">
+              <Col md={6}>
+                <p>{god.abilities[1].name}</p>
+                <p>{god.abilities[1].description}</p>
+                {god.abilities[1].details.length > 0 ? (
+                  <div>
+                    {god.abilities[1].details.map((det, indice) => {
+                      return (
+                        <p key={indice}>
+                          {det.label}: {det.value}
+                        </p>
+                      );
+                    })}
+                  </div>
+                ) : (
+                  <p></p>
+                )}
+              </Col>
+              <Col md={6}>
+                <img src={god.images.card} />
+              </Col>
+            </Row>
 
-          <Row className="caja">
-            <Col md={6}>
-              <img src={god.images.card} />
-            </Col>
-            <Col md={6}>
-              <p>{god.abilities[2].name}</p>
-              <p>{god.abilities[2].description}</p>
-              {god.abilities[2].details.length > 0 ? (
-                <div>
-                  {god.abilities[2].details.map((det, indice) => {
-                    return (
-                      <p key={indice}>
-                        {det.label}: {det.value}
-                      </p>
-                    );
-                  })}
-                </div>
-              ) : (
-                <p></p>
-              )}
-            </Col>
-          </Row>
-          <Row className="caja">
-            <Col md={6}>
-              <p>{god.abilities[3].name}</p>
-              <p>{god.abilities[3].description}</p>
-              {god.abilities[3].details.length > 0 ? (
-                <div>
-                  {god.abilities[3].details.map((det, indice) => {
-                    return (
-                      <p key={indice}>
-                        {det.label}: {det.value}
-                      </p>
-                    );
-                  })}
-                </div>
-              ) : (
-                <p></p>
-              )}
-            </Col>
-            <Col md={6}>
-              <img src={god.images.card} />
-            </Col>
-          </Row>
-          <Row className="caja">
-            <Col md={6}>
-              <img src={god.images.card} />
-            </Col>
-            <Col md={6}>
-              <p>{god.abilities[4].name}</p>
-              <p>{god.abilities[4].description}</p>
-              {god.abilities[4].details.length > 0 ? (
-                <div>
-                  {god.abilities[4].details.map((det, indice) => {
-                    return (
-                      <p key={indice}>
-                        {det.label}: {det.value}
-                      </p>
-                    );
-                  })}
-                </div>
-              ) : (
-                <p></p>
-              )}
-            </Col>
-          </Row>
-        </Container>
+            <Row className="caja">
+              <Col md={6}>
+                <img src={god.images.card} />
+              </Col>
+              <Col md={6}>
+                <p>{god.abilities[2].name}</p>
+                <p>{god.abilities[2].description}</p>
+                {god.abilities[2].details.length > 0 ? (
+                  <div>
+                    {god.abilities[2].details.map((det, indice) => {
+                      return (
+                        <p key={indice}>
+                          {det.label}: {det.value}
+                        </p>
+                      );
+                    })}
+                  </div>
+                ) : (
+                  <p></p>
+                )}
+              </Col>
+            </Row>
+            <Row className="caja">
+              <Col md={6}>
+                <p>{god.abilities[3].name}</p>
+                <p>{god.abilities[3].description}</p>
+                {god.abilities[3].details.length > 0 ? (
+                  <div>
+                    {god.abilities[3].details.map((det, indice) => {
+                      return (
+                        <p key={indice}>
+                          {det.label}: {det.value}
+                        </p>
+                      );
+                    })}
+                  </div>
+                ) : (
+                  <p></p>
+                )}
+              </Col>
+              <Col md={6}>
+                <img src={god.images.card} />
+              </Col>
+            </Row>
+            <Row className="caja">
+              <Col md={6}>
+                <img src={god.images.card} />
+              </Col>
+              <Col md={6}>
+                <p>{god.abilities[4].name}</p>
+                <p>{god.abilities[4].description}</p>
+                {god.abilities[4].details.length > 0 ? (
+                  <div>
+                    {god.abilities[4].details.map((det, indice) => {
+                      return (
+                        <p key={indice}>
+                          {det.label}: {det.value}
+                        </p>
+                      );
+                    })}
+                  </div>
+                ) : (
+                  <p></p>
+                )}
+              </Col>
+            </Row>
+          </Container>
+        </>
       ) : (
         <div>
           <p>Sin datos</p>
