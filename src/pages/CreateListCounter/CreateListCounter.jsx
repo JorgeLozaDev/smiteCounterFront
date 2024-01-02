fe39a6a9a33bc1d4111fa7b8506d3087685b18ce
jorge <j0rg3e93@gmail.com>
2024-01-02 23:34:55 +0100

    UPDATE: Se cambia la estructura y la logica de la pagina createlistcounters

--- src/pages/CreateListCounter/CreateListCounter.jsx
+++ src/pages/CreateListCounter/CreateListCounter.jsx
@@ -1,132 +1,111 @@
-// CreateListCounter.js
-import React from 'react';
-import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { userDetails } from "../userSlice";
+import { useSelector } from "react-redux";
+import { allGodsActives } from "../../services/apiCalls";
+import { Col, Row, Container, Button } from "react-bootstrap";
+import CustomSelect from "../../common/CustomSelect/CustomSelect";
+import "./CreateListCounter.css";
 
 const CreateListCounter = () => {
-  const initialItems = [
-    { id: 'item-1', content: 'Item 1' },
-    { id: 'item-2', content: 'Item 2' },
-    { id: 'item-3', content: 'Item 3' },
-    // ...add more items as needed
-  ];
+  const [rows, setRows] = useState([]);
+  const [gods1, setGods1] = useState([]);
+  const [selectedGod, setSelectedGod] = useState(null);
+  const [rowCount, setRowCount] = useState(0); // Nuevo estado para contar las filas
+  const navigate = useNavigate();
+  const token = useSelector(userDetails);
 
-  const [items, setItems] = React.useState(initialItems);
-  const [rows, setRows] = React.useState([]);
+  useEffect(() => {
+    if (token.credentials === "") {
+      navigate("/");
+    }
 
-  const onDragEnd = (result) => {
-    if (!result.destination) return;
+    allGodsActives("gods/allGodsActive")
+      .then((data) => {
+        setGods1(data.data.allGodsActive);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
-    const updatedItems = Array.from(items);
-    const [movedItem] = updatedItems.splice(result.source.index, 1);
-    updatedItems.splice(result.destination.index, 0, movedItem);
-
-    setItems(updatedItems);
-  };
-
-  const addRow = () => {
+  const handleButtonClick = () => {
     const newRow = {
-      id: `row-${rows.length + 1}`,
-      columns: [
-        {
-          id: `col-1-${rows.length + 1}`,
-          items: [
-            { id: `item-col1-${rows.length + 1}-1`, content: 'Column 1 Item 1' },
-            { id: `item-col1-${rows.length + 1}-2`, content: 'Column 1 Item 2' },
-            // ...add more items for column 1 as needed
-          ],
-        },
-        {
-          id: `col-2-${rows.length + 1}`,
-          items: [
-            { id: `item-col2-${rows.length + 1}-1`, content: 'Column 2 Item 1' },
-            { id: `item-col2-${rows.length + 1}-2`, content: 'Column 2 Item 2' },
-            // ...add more items for column 2 as needed
-          ],
-        },
+      id: `row-${rowCount + 1}`, // Utilizando rowCount como identificador único
+      cols: [
+        { id: `col-1`, size: 3 },
+        { id: `col-2`, size: 6 },
+        { id: `col-3`, size: 3 },
       ],
     };
 
+    setRowCount((prevCount) => prevCount + 1); // Incrementando el contador
     setRows((prevRows) => [...prevRows, newRow]);
   };
 
+  const handleDeleteRow = (rowId) => {
+    setRows((prevRows) => prevRows.filter((row) => row.id !== rowId));
+  };
+
+  const inputHandler = () => {};
+
+  const handleGodSelect = (selectedOption) => {
+
+    const selectedGodDetails = gods1.find(
+      (god) => god._id === selectedOption
+    );
+    setSelectedGod(selectedGodDetails);
+  };
+
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col-12">
-          <DragDropContext onDragEnd={onDragEnd}>
-            <Droppable droppableId="list">
-              {(provided) => (
-                <ul
-                  {...provided.droppableProps}
-                  ref={provided.innerRef}
-                  className="list-group"
-                >
-                  {items.map((item, index) => (
-                    <Draggable key={item.id} draggableId={item.id} index={index}>
-                      {(provided) => (
-                        <li
-                          ref={provided.innerRef}
-                          {...provided.draggableProps}
-                          {...provided.dragHandleProps}
-                          className="list-group-item"
-                        >
-                          {item.content}
-                        </li>
-                      )}
-                    </Draggable>
-                  ))}
-                  {provided.placeholder}
-                </ul>
-              )}
-            </Droppable>
-          </DragDropContext>
-        </div>
-      </div>
-      <div className="row mt-3">
-        <div className="col-12">
-          <button className="btn btn-primary" onClick={addRow}>
-            Agregar Fila
-          </button>
-        </div>
-      </div>
-      {rows.map((row) => (
-        <div className="row mt-3" key={row.id}>
-          {row.columns.map((col) => (
-            <div className="col-md-6" key={col.id}>
-              <Droppable droppableId={col.id} key={col.id}>
-                {(provided) => (
-                  <ul
-                    {...provided.droppableProps}
-                    ref={provided.innerRef}
-                    className="list-group"
-                  >
-                    {col.items.map((item, index) => (
-                      <Draggable
-                        key={item.id}
-                        draggableId={item.id}
-                        index={index}
-                      >
-                        {(provided) => (
-                          <li
-                            ref={provided.innerRef}
-                            {...provided.draggableProps}
-                            {...provided.dragHandleProps}
-                            className="list-group-item"
-                          >
-                            {item.content}
-                          </li>
-                        )}
-                      </Draggable>
-                    ))}
-                    {provided.placeholder}
-                  </ul>
+    <>
+      <Container>
+        <Row>
+          <Col>
+            <Button onClick={handleButtonClick}>Agregar Fila</Button>
+          </Col>
+        </Row>
+
+        {rows.map((row) => (
+          <div className="row cajaCounters" key={row.id}>
+            {row.cols.map((col, index) => (
+              <div key={`${col.id}-${index}`} className={`col-md-${col.size}`}>
+                {/* Content for each column */}
+                {index === 0 && (
+                  <div key={`selected-god-${row.id}`} className="text-center">
+                    {selectedGod && (
+                      <img
+                        src={selectedGod.images.card}
+                        alt="Dios Seleccionado"
+                        className="img-principal-counter"
+                      />
+                    )}
+                    <CustomSelect
+                      options={gods1.map((god) => ({
+                        value: god._id,
+                        label: god.name,
+                      }))}
+                      placeholder="Seleccione un dios"
+                      name="god"
+                      handler={handleGodSelect}
+                      className=""
+                    />
+                  </div>
                 )}
-              </Droppable>
-            </div>
-          ))}
-        </div>
-      ))}
-    </div>
+                {index === row.cols.length - 1 && (
+                  <div key={`buttons-${row.id}`}>
+                    <Button onClick={() => handleDeleteRow(row.id)}>
+                      Eliminar
+                    </Button>
+                    <Button>Guardar</Button>
+                  </div>
+                )}
+              </div>
+            ))}
+          </div>
+        ))}
+      </Container>
+    </>
   );
 };
 
